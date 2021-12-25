acc92930f0d4fdfc69531090eed7eda29b5fdc31
Ivan <fedivmi@gmail.com>
2021-12-26 00:11:29 +0300

    added camera

--- app/src/main/java/com/example/android_image_editor/MainActivity.kt
+++ app/src/main/java/com/example/android_image_editor/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.android_image_editor
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var imageView: ImageView
+    lateinit var  btnOpenCamera: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.image_view)
+        btnOpenCamera = findViewById(R.id.btn_open_camera)
+
+        checkCameraPermissions()
+
+        btnOpenCamera.setOnClickListener {
+            openCamera()
+        }
     }
+
+    private fun checkCameraPermissions() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 100)
+        }
+    }
+
+    private fun openCamera(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, 100)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 100 && data != null && data.extras != null){
+            val capturedImage = data.extras!!.get("data") as Bitmap
+            imageView.setImageBitmap(capturedImage)
+        }
+    }
+
 }
